8db6e956467fb24d78b389445a60a4100032ab31
Philip Norton <phil.norton@codeenigma.com>
2026-02-15 11:33:51 +0000

    Simplified the select-oob example.

--- public/usage/select-oob/index.php
+++ public/usage/select-oob/index.php
@@ -3,6 +3,6 @@
 require '../../Htmx.php';
 
 if (Htmx::isHtmxRequest() && Htmx::isGet()) {
-  echo '<button hx-get="index.php" hx-select-oob="#div1" hx-swap="outerHTML">Request Sent</button>';
+  echo 'Request Sent';
   echo '<div id="div1">Button clicked at ' . date('r') . '.</div>';
 }
